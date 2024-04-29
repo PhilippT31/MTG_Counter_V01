80478ed17918d8e7dbb02a1682c401a806c1d455
PhilippT31 <120516138+PhilippT31@users.noreply.github.com>
2024-04-29 19:43:32 +0200

    Implementation of starting-Attribute Implementation of select_starter function to choose the starting player

--- src.cpp
+++ src.cpp
@@ -13,6 +13,9 @@ public:
 	int cmd_dmg;
 	int toxic_cnt;
 	bool out_of_cards;
+	bool starting;
+
+	players() = default;
 };
 
 //init all players
@@ -26,32 +29,32 @@ player1.life_cnt = lifecount_start;
 player1.cmd_dmg = 0;
 player1.toxic_cnt = 0;
 player1.out_of_cards = false;
+player1.starting = false;
 
 player2.life_cnt = lifecount_start;
 player2.cmd_dmg = 0;
 player2.toxic_cnt = 0;
 player2.out_of_cards = false;
+player2.starting = false;
 
 player3.life_cnt = lifecount_start;
 player3.cmd_dmg = 0;
 player3.toxic_cnt = 0;
 player3.out_of_cards = false;
+player3.starting = false;
 
 player4.life_cnt = lifecount_start;
 player4.cmd_dmg = 0;
 player4.toxic_cnt = 0;
 player4.out_of_cards = false;
+player4.starting = false;
+
 
 player5.life_cnt = lifecount_start;
 player5.cmd_dmg = 0;
 player5.toxic_cnt = 0;
 player5.out_of_cards = false;
-
-//game
-const int cmd_dmg_max = 21;
-const int toxic_cnt_max = 10;
-const int min_players = 2;
-const int max_players = 5;
+player5.starting = false;
 
 //inits
 int player_cnt = 0;
@@ -59,6 +62,12 @@ int a = 0;
 int lifecount_start = 40;
 int rounds_cnt = 0;
 
+//game
+const int cmd_dmg_max = 21;
+const int toxic_cnt_max = 10;
+const int min_players = 2;
+const int max_players = 5;
+public int playorder[player_cnt];
 
 void main() {
 	//entering the number of players
@@ -78,9 +87,11 @@ void main() {
 
 	// declaring the starter by random choice
 
-	int starter = rand() % player_cnt + min_players;
+	select_starter();
 
 	// start of game
+
+	define_playorder();
 
 	// the game
 
@@ -129,6 +140,26 @@ void naming_players(a) {
 		case(2):
 			cout << "Enter the name of player 2: ";
 			cin >> player2.name;
-	}
+	};
 		
+}
+
+void select_starter() {
+	int starter = rand() (% player_cnt + min_players);
+	switch (starter) {
+		case(1):
+			player1.starting = true;
+		case(2):
+			player2.starting = true;
+		case(3):
+			player3.starting = true;
+		case(4):
+			player4.starting = true;
+		case(5):
+			player5.starting = true;
+	};
+}
+
+void define_playorder(){
+	
 }
